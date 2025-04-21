ffd4b1f28c56e26f0bea612b8dc3e2baa6ad696d
Pedro <pa.gouveia@campus.fct.unl.pt>
2025-04-21 15:14:21 +0100

    Updated credits and splash screens
    - Retired old splash screen logic
    - Organized UI assets folder

---
+++ Assets/Scripts/Controllers/SplashScreenController.cs
@@ -0,0 +1,67 @@
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreenController : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] splashImages;
+    public float fadeDuration;
+    [SerializeField]
+    private int splashCounter;
+
+    void Awake()
+    {
+     splashImages = GetComponentsInChildren<Image>();
+     splashCounter = 0; 
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //makes splash screens invisible
+        foreach (var image in splashImages) {
+            image.color = new Color(255f, 255f, 255f, 0f);
+        }
+
+        StartCoroutine(displaySplashImage());
+    }
+
+    private IEnumerator displaySplashImage() {
+        Image image = splashImages[splashCounter];
+        float alpha = 0f;
+        float elapsedTime = 0f;
+
+        //fade in
+        while(elapsedTime < fadeDuration) {
+            elapsedTime += Time.deltaTime;
+            alpha = Mathf.Clamp01(elapsedTime/fadeDuration);
+            image.color = new Color(255f, 255f, 255f, alpha);
+            yield return null;
+        }
+        image.color = new Color(255f, 255f, 255f, 1f);
+
+        //wait a bit
+        yield return new WaitForSeconds(fadeDuration);
+
+        //fade out
+        while(elapsedTime > 0f) {
+            elapsedTime -= Time.deltaTime;
+            alpha = Mathf.Clamp01(elapsedTime/fadeDuration);
+            image.color = new Color(255f, 255f, 255f, alpha);
+            yield return null;
+        }
+
+        //display next splash
+        if(++splashCounter < splashImages.Length) {
+            StartCoroutine(displaySplashImage());
+        }
+        else {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().loadMainMenuScene();
+        }
+        yield return null;
+
+    }
+}
